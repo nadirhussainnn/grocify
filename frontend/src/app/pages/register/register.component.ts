f246033e024425e9804fe1053c53ea1621f5682c
zakirhakro12 <hakrozakir@gmail.com>
2025-07-09 02:33:28 +0200

    update: consumed APIs

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -11,4 +12,21 @@ import { CommonModule } from '@angular/common';
 })
 export class RegisterComponent {
 
-}
+    email = signal('');
+    password = signal('');
+    error = signal('');
+  
+    constructor(private auth: AuthService, private router: Router) {}
+  
+    handleRegister() {
+      this.auth.registerCustomer(this.email(), this.password()).subscribe({
+        next: (res) => {
+          this.router.navigate(['/login']);
+        },
+        error: () => {
+          this.error.set('Failed to register - try again with other email!');
+        }
+      });
+    }
+  }
+  
